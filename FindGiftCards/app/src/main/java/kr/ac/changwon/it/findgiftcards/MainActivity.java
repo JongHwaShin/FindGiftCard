f151252e971c3c0fb0588f482f2cecf77785c1a2
Ganjang-chichen <59672593+Ganjang-chichen@users.noreply.github.com>
2021-03-12 13:24:49 +0900

    first commit
    readme update

---
+++ FindGiftCards/app/src/main/java/kr/ac/changwon/it/findgiftcards/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.changwon.it.findgiftcards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
